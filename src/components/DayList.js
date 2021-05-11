a028852eb806e16713b62ddb959c3c1bd7919bd8
Grigor-ship-it <gb795@hotmail.com>
2021-05-10 20:50:01 -0400

    new DayList component functional with styling

---
+++ src/components/DayList.js
@@ -0,0 +1,16 @@
+import React from "react";
+import DayListItem from "components/DayListItem"
+
+
+
+
+
+export default function DayList(props) {
+  const { days } = props
+  
+
+  const parsedDays = days && days.map(day => <DayListItem key={day.id} {...day} setDay={props.setDay} selected={props.day === day.name} />)
+    return (
+      <ul>{parsedDays}</ul>
+    );
+  }
