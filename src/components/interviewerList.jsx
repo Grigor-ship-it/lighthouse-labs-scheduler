461c930e682349c336b025d2585774dd515ef0bf
Grigor-ship-it <gb795@hotmail.com>
2021-05-11 13:54:02 -0400

    interviewerList is now functional including css elements

---
+++ src/components/interviewerList.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import InterviewerListItem from "components/InterviewerListItem"
+
+import "components/interviewerList.scss"
+
+
+
+
+
+export default function InterviewerList(props) {
+  const { interviewers } = props
+  
+
+  const parsedInterviewers = interviewers && interviewers.map(interviewer => <InterviewerListItem key={interviewer.id} {...interviewer} 
+    setInterviewer={props.setInterviewer} selected={props.interviewer=== interviewer.id} avatar={interviewer.avatar}/>)
+    return (
+      <section className="interviewers">
+        <h4 className="interviewers__header text--light">Interviewer</h4>
+        <ul className="interviewers__list">{parsedInterviewers}</ul>
+      </section>
+    );
+  }
