0769ffebc9ab25debf33633eb01786592da62d2c
Grigor-ship-it <gb795@hotmail.com>
2021-05-13 20:45:56 -0400

    Appointment component is updated with CREATE/EMPTY/SHOW conditionals

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -2,17 +2,41 @@ import React from "react";
 import Header from "components/Appointment/header"
 import Empty from "components/Appointment/empty"
 import Show from "components/Appointment/show"
+import UseVisualMode from "hooks/useVisualMode"
+import Form from "components/Appointment/form"
 
 import "components/Appointment/styles.scss";
 
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
+const CONFIRM = "CONFIRM"
+
+
 export default function Appointment(props) {
 
+  
   const {student, interview} = props
+ 
+  
+
+  
+
+  const { mode, transition, back } = UseVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
   
     return (
       <article className="appointment">
       <Header time={props.time}/>
-      {props.interview ? <Show student={interview.student} interviewer={interview.interviewer}/> : <Empty />}
+      {mode === CREATE && <Form onSave={() => transition(CONFIRM)} onCancel={() => back(EMPTY)}/>}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} onCancel={() => back(EMPTY)}/>}
+{mode === SHOW && (
+  <Show
+    student={props.interview.student}
+    interviewer={props.interview.interviewer}
+  />
+)}
       </article>
     );
   }
--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,6 +1,6 @@
 import { useState } from "react"
 
-export default function useVisualMode(initial) {
+export default function UseVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]); 
 
